166c2c423106c67396da846c2c56c9354576efab
Serge Manvelov <kawaiier@tutanota.com>
2026-02-02 18:26:53 +0700

    Updated project name and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Todo App",
-  description: "The best todo app",
+  title: "Save Me",
+  description: "Stressless Task Management",
 };
 
 export default function RootLayout({
@@ -13,9 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
